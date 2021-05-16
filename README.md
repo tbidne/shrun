da9d9b55bb2daf3314ec37e11f7c5908657ac5da
Tommy Bidne <tbidne@protonmail.com>
2021-05-16 17:05:12 +1200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shell Runner
+
+Runs arbitrary shell commands asynchronously.
---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ compiler ? "ghc8104"
+, pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ad47284f8b01f587e24a4f14e0f93126d8ebecda.tar.gz") {}
+}:
+
+let
+  haskellDeps = ps: with ps; [
+    implicit-hie
+  ];
+
+  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages haskellDeps;
+in pkgs.mkShell {
+
+  buildInputs = [
+    ghc
+  ];
+
+  shellHook = ''
+    alias dev-build='cabal v2-build --ghc-options="-Wwarning"'
+    alias dev-test='cabal v2-test --ghc-options="-Wwarning"'
+    alias dev-run='cabal v2-run euler --ghc-options="-Wwarning"'
+  '';
+}
