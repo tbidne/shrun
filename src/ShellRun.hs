9ce5bc7b1ce787cf0509be767f13fa7630d4123f
Tommy Bidne <tbidne@protonmail.com>
2021-05-20 14:27:20 +1200

    Rename cabal job, clean up runshell logic

--- src/ShellRun.hs
+++ src/ShellRun.hs
@@ -5,6 +5,7 @@ module ShellRun
   )
 where
 
+import Data.Text (Text)
 import Data.Text qualified as T
 import ShellRun.Class.MonadLogger (MonadLogger)
 import ShellRun.Class.MonadLogger qualified as ML
@@ -12,19 +13,21 @@ import ShellRun.Class.MonadShell (MonadShell (..))
 import ShellRun.Parsing.Commands qualified as ParseCommands
 import ShellRun.Types.Args (Args (..))
 import ShellRun.Types.Command (Command (..))
+import ShellRun.Types.Legend (LegendErr)
+import ShellRun.Types.NonNegative (NonNegative)
 
--- TODO: improve this
 runShell :: (MonadLogger m, MonadShell m) => m ()
 runShell = do
   MkArgs {legend, timeout, commands} <- parseArgs
-  maybeCommands <- case legend of
-    Just path -> do
-      legendMap <- legendPathToMap path
-      pure $ case legendMap of
-        Right mp -> Right $ ParseCommands.translateCommands mp commands
-        Left err -> Left err
-    Nothing -> pure $ Right $ fmap MkCommand commands
+  parsedCommands <- maybePathToCommands legend commands
+  runCommandsOrLogErr parsedCommands timeout
 
-  case maybeCommands of
-    Right cmds -> runCommands cmds timeout
-    Left err -> ML.logError $ T.pack $ show err
+maybePathToCommands :: MonadShell m => Maybe Text -> [Text] -> m (Either LegendErr [Command])
+maybePathToCommands Nothing commands = pure $ Right $ fmap MkCommand commands
+maybePathToCommands (Just path) commands = do
+  lMap <- legendPathToMap path
+  pure $ fmap (`ParseCommands.translateCommands` commands) lMap
+
+runCommandsOrLogErr :: (MonadLogger m, MonadShell m) => Either LegendErr [Command] -> Maybe NonNegative -> m ()
+runCommandsOrLogErr (Right cmds) timeout = runCommands cmds timeout
+runCommandsOrLogErr (Left err) _ = ML.logError $ T.pack $ show err
