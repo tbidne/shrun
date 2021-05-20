f82517d738957975f13fa99db03fb857f915c504
Tommy Bidne <tbidne@protonmail.com>
2021-05-21 11:16:27 +1200

    Add failure property test for parsing legend file

--- src/ShellRun/Parsing/Legend/Internal.hs
+++ src/ShellRun/Parsing/Legend/Internal.hs
@@ -22,6 +22,8 @@ linesToMap = foldr f (Right Map.empty)
 parseLine :: Text -> Either LegendErr (Text, Text)
 parseLine l =
   case T.break (== '=') l of
+    -- This is actually impossible for a non-empty string, which `l`
+    -- /should/ be by this point, but including it for completeness...
     ("", _) -> Left $ ParseErr $ "Key cannot be empty: " <> l
     (_, "") -> Left $ ParseErr $ "Value cannot be empty: " <> l
     -- T.tail is safe because v can't be empty, or it would have matched
