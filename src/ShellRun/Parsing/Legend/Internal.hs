52fc15174245e16aee6ee06e75b1cae5b2b86fa0
Tommy Bidne <tbidne@protonmail.com>
2021-05-26 13:08:31 +1200

    Add Utils.Text props

--- src/ShellRun/Parsing/Legend/Internal.hs
+++ src/ShellRun/Parsing/Legend/Internal.hs
@@ -11,7 +11,6 @@ import Data.Map.Strict qualified as Map
 import Data.Text (Text)
 import Data.Text qualified as T
 import ShellRun.Types.Legend (LegendErr (..), LegendMap)
-import ShellRun.Utils qualified as Utils
 import ShellRun.Utils.Text qualified as TextUtils
 
 linesToMap :: [Text] -> Either LegendErr LegendMap
@@ -27,9 +26,9 @@ linesToMap = foldr f (Right Map.empty)
 
 parseLine :: Text -> Either LegendErr (Text, Text)
 parseLine l =
-  case TextUtils.breakStripPoint break l of
+  case TextUtils.breakStripPoint breakPoint l of
     ("", _) -> Left $ EntryErr $ "Key cannot be empty: " <> l
     (_, "") -> Left $ EntryErr $ "Value cannot be empty: " <> l
     (k, v) -> Right (k, v)
   where
-    break = TextUtils.unsafeMkNonEmptyText "="
+    breakPoint = TextUtils.unsafeMkNonEmptyText "="
---
+++ test/unit/Props/ShellRun/Utils/Text.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
+module Props.ShellRun.Utils.Text (props) where
+
+import Hedgehog ((===))
+import Hedgehog qualified as H
+import Props.Generators qualified as PGens
+import ShellRun.Utils.Text qualified as TextUtils
+import Test.Tasty (TestTree)
+import Test.Tasty qualified as T
+import Test.Tasty.Hedgehog qualified as TH
+import Data.Text qualified as Txt
+
+props :: TestTree
+props = T.testGroup "ShellRun.Utils.Text" [mkNonEmptyText, unsafeNonEmptyText]
+
+mkNonEmptyText :: TestTree
+mkNonEmptyText = TH.testProperty "mkNonEmptyText only succeeds on non-empty input" $
+  H.property $ do
+    t <- H.forAll PGens.genText
+    let result = TextUtils.mkNonEmptyText t
+    H.assert $ case result of
+      Just _ -> Txt.length t > 0
+      Nothing -> Txt.length t == 0
+
+unsafeNonEmptyText :: TestTree
+unsafeNonEmptyText = TH.testProperty "unsafeNonEmptyText succeeds on non-empty input" $
+  H.property $ do
+    t <- H.forAll PGens.getNonEmptyText
+    let rawText = TextUtils.unNonEmptyText t
+    -- Silly assertion since we can't try with empty text, but basically tests:
+    --   1. We don't get a runtime error with non-empty text
+    --   2. We don't receive empty text
+    t === TextUtils.unsafeMkNonEmptyText rawText
+    H.assert $ Txt.length rawText > 0
