f653965ab6e81b8a1df56e978d2374bafef0f112
Tommy Bidne <tbidne@protonmail.com>
2021-06-02 17:52:02 +1200

    Rename doc/ -> docs/

--- scripts/gen_haddock.sh
+++ scripts/gen_haddock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rm -rf ./doc/*
+rm -rf ./docs/*
 cabal haddock --haddock-html --haddock-hyperlink-source
-cp -r dist-newstyle/build/x86_64-linux/ghc-8.10.4/shell-run-0.1.0.0/doc/html/shell-run/* ./doc
+cp -r dist-newstyle/build/x86_64-linux/ghc-8.10.4/shell-run-0.1.0.0/doc/html/shell-run/* ./docs
