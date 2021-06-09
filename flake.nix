038c0b0b3efb966aa79d228676ccfc53afcec031
Tommy Bidne <tbidne@protonmail.com>
2021-06-09 23:21:23 +1200

    Add flakes support

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "shell-run flake";
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        mkPkg = returnShellEnv:
+          pkgs.haskellPackages.developPackage {
+            inherit returnShellEnv;
+            name = "shell-run";
+            root = ./.;
+            modifier = drv:
+              pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages; [
+                cabal-fmt
+                cabal-install
+                haskell-language-server
+                hlint
+                implicit-hie
+                ormolu
+                pkgs.nixpkgs-fmt
+              ]);
+          };
+      in
+      {
+        defaultPackage = mkPkg false;
+
+        devShell = mkPkg true;
+      });
+}
--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 { compiler ? "ghc8104"
-, pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ad47284f8b01f587e24a4f14e0f93126d8ebecda.tar.gz") {}
+, pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ad47284f8b01f587e24a4f14e0f93126d8ebecda.tar.gz") { }
 }:
 
 let
   haskellDeps = ps: with ps; [
+    cabal-install
+    cabal-fmt
     hlint
     implicit-hie
   ];
@@ -15,12 +17,13 @@ let
   ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages haskellDeps;
 
   otherDeps = [
-    pkgs.cabal-install
+    pkgs.nixpkgs-fmt
   ];
-in pkgs.mkShell {
+in
+pkgs.mkShell {
 
   buildInputs =
-    [ghc]
+    [ ghc ]
     ++ haskellOtherDeps
     ++ otherDeps;
 
