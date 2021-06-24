eb42154a7e0f924dc96eb714653bd22e15aabdc9
Tommy Bidne <tbidne@protonmail.com>
2021-06-24 23:15:29 +1200

    Fix doctest paths

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -12,7 +12,7 @@ main =
       "src/ShellRun/Math/Positive.hs",
       "src/ShellRun/Parsing/Commands.hs",
       "src/ShellRun/Parsing/Legend/Internal.hs",
-      "ShellRun.Utils.Internal",
-      "ShellRun.Utils",
+      "src/ShellRun/Utils/Internal.hs",
+      "src/ShellRun/Utils.hs",
       "src/ShellRun/Utils/Text.hs"
     ]
