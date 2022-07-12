4140436cf7d8e21eda9e37a40d6b36467f5a9f35
Tommy Bidne <tbidne@protonmail.com>
2022-07-13 11:46:52 +1200

    Update algebra-simple, remove env-guard from flakes

--- flake.nix
+++ flake.nix
@@ -2,14 +2,12 @@
   description = "Shell-Run is a tool for concurrently running shell commands.";
   inputs = {
     algebra-simple-src.url = "github:tbidne/algebra-simple";
-    env-guard-src.url = "github:tbidne/env-guard";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     relative-time-src.url = "github:tbidne/relative-time";
   };
   outputs =
     { algebra-simple-src
-    , env-guard-src
     , flake-utils
     , nixpkgs
     , relative-time-src
@@ -35,8 +33,6 @@
           overrides = final: prev: with compiler; {
             algebra-simple =
               final.callCabal2nix "algebra-simple" algebra-simple-src { };
-            env-guard =
-              final.callCabal2nix "env-guard" env-guard-src { };
             package-version = pkgs.haskell.lib.doJailbreak prev.package-version;
             relative-time =
               final.callCabal2nix "relative-time" relative-time-src { };
