ef6b280e6ea094036be99356e1b969006ee6dd3e
Tommy Bidne <tbidne@protonmail.com>
2023-02-22 10:51:14 +1300

    Swap doctest-parallel for doctest

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -9,5 +9,47 @@ main =
   guardOrElse'
     "RUN_DOCTEST"
     ExpectEnvSet
-    (DocTest.mainFromCabal "shrun" [])
+    (DocTest.doctest args)
     (putStrLn "*** Doc Tests disabled. Enable with RUN_DOCTEST=1")
+  where
+    args = files <> exts
+
+files :: [String]
+files =
+  [ "-isrc",
+    "src/Shrun/Configuration/Legend.hs",
+    "src/Shrun/Data/Command.hs",
+    "src/Shrun/Data/PollInterval.hs",
+    "src/Shrun/Logging/Formatting.hs",
+    "src/Shrun/Prelude.hs",
+    "src/Shrun/Utils.hs"
+  ]
+
+exts :: [String]
+exts =
+  [ "-XNoImplicitPrelude",
+    "-XNoStarIsType",
+    "-XApplicativeDo",
+    "-XBangPatterns",
+    "-XDataKinds",
+    "-XDeriveGeneric",
+    "-XDerivingVia",
+    "-XDuplicateRecordFields",
+    "-XFlexibleContexts",
+    "-XFlexibleInstances",
+    "-XImportQualifiedPost",
+    "-XInstanceSigs",
+    "-XLambdaCase",
+    "-XMultiParamTypeClasses",
+    "-XMultiWayIf",
+    "-XNamedFieldPuns",
+    "-XNumericUnderscores",
+    "-XOverloadedLabels",
+    "-XOverloadedStrings",
+    "-XPatternSynonyms",
+    "-XScopedTypeVariables",
+    "-XStandaloneKindSignatures",
+    "-XTupleSections",
+    "-XTypeApplications",
+    "-XTypeFamilies"
+  ]
