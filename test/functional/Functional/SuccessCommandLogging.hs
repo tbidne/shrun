0d2da64432052d5322258a817edea58c23aaba62
Tommy Bidne <tbidne@protonmail.com>
2023-03-10 12:00:49 +1300

    Add test for stderr capture

--- test/functional/Functional/SuccessCommandLogging.hs
+++ test/functional/Functional/SuccessCommandLogging.hs
@@ -9,43 +9,66 @@ import Test.Shrun.Verifier qualified as V
 
 -- | Spec that should run commands successfully and print stdout.
 spec :: IO TestArgs -> TestTree
-spec args = withResource (pure ()) (teardown args) $ \_ ->
+spec args =
   testGroup
     "Command logging tests"
-    [ testCase "Should print commands stdout" $ do
-        MkTestArgs {tmpDir} <- args
-        let outpath = tmpDir </> outfile
-            argList =
-              [ -- Need file logging to read commands
-                "-f" <> outpath,
-                "--no-config",
-                "sleep 2",
-                cmdLogging
-              ]
-                <> commands
-
-        _ <- run argList
-
-        fileResult <- readFileUtf8Lenient outpath
-        let results = MkResultText <$> T.lines fileResult
-        V.verifyExpected results expected
+    [ success args,
+      capturesStderr
     ]
+
+success :: IO TestArgs -> TestTree
+success args = withResource (pure ()) teardown $ \_ ->
+  testCase "Should print commands stdout" $ do
+  MkTestArgs {tmpDir} <- args
+  let outpath = tmpDir </> outfile
+      argList =
+        [ -- Need file logging to read commands
+          "-f" <> outpath,
+          "--no-config",
+          "sleep 2",
+          cmdLogging
+        ]
+          <> commands
+
+  _ <- run argList
+
+  fileResult <- readFileUtf8Lenient outpath
+  let results = MkResultText <$> T.lines fileResult
+  V.verifyExpected results expected
   where
     -- `sleep 1` is because commands that run too quickly will not have
     -- output logged
     commands = ["echo hi && sleep 1"]
     cmdLogging = "--cmd-log"
 
-expected :: List ExpectedText
-expected =
-  MkExpectedText
-    <$> [ withCommandPrefix "echo hi && sleep 1" "hi"
+    expected =
+      MkExpectedText
+        <$> [ withCommandPrefix "echo hi && sleep 1" "hi"
+            ]
+
+    outfile :: FilePath
+    outfile = "cmd_log"
+
+    teardown :: () -> IO ()
+    teardown _ = do
+      MkTestArgs {tmpDir} <- args
+      void $ tryAny $ removeFileIfExists (tmpDir </> outfile)
+
+capturesStderr :: TestTree
+capturesStderr = testCase "Should capture stdout and stderr" $ do
+  let argList =
+        [ "--no-config",
+          "--cmd-log",
+          "./test/functional/Functional/stderr.sh"
         ]
 
-outfile :: String
-outfile = "cmd_log"
+  results <- fmap MkResultText <$> (readIORef =<< run argList)
 
-teardown :: IO TestArgs -> () -> IO ()
-teardown args _ = do
-  MkTestArgs {tmpDir} <- args
-  void $ tryAny $ removeFileIfExists (tmpDir </> outfile)
+  V.verifyExpected results expected
+  where
+    expected =
+      [ withCommandPrefix "./test/functional/Functional/stderr.sh" "stdout 1",
+        withCommandPrefix "./test/functional/Functional/stderr.sh" "stderr 1",
+        withCommandPrefix "./test/functional/Functional/stderr.sh" "stdout 2",
+        withCommandPrefix "./test/functional/Functional/stderr.sh" "stderr 2"
+      ]
---
+++ test/functional/Functional/stderr.sh
@@ -0,0 +1,17 @@
+sleep 1
+
+echo "stdout 1"
+
+sleep 1
+
+>&2 echo "stderr 1"
+
+sleep 1
+
+echo "stdout 2"
+
+sleep 1
+
+>&2 echo "stderr 2"
+
+sleep 1
