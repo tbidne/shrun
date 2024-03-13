d61b928102dbf9bdf3b1c73d0daaf50ceaff6adc
Tommy Bidne <tbidne@protonmail.com>
2024-03-14 12:16:51 +1300

    Fix buffering functional test

--- test/functional/Functional/Buffering.hs
+++ test/functional/Functional/Buffering.hs
@@ -21,27 +21,35 @@ logsNoBuffer =
 
     assertLogsEq expecteds results
   where
+    -- NOTE: [Bash brace loop interpolation]
+    --
+    -- The bash loop brace syntax "for i in {0..3}" does not seem to
+    -- interpolate correctly with nix or on CI. The brace is interpreted
+    -- literally i.e. {0..3}. The manual listing of indices appears to work,
+    -- however
     args =
       [ "--cmd-log",
         "--cmd-log-strip-control=all",
-        "for i in {0..3}; do echo $i; sleep 1.5; done"
+        "--cmd-name-trunc=15",
+        "--no-config",
+        "for i in 0 1 2 3; do echo \"$i\"; sleep 1.5; done"
       ]
 
     -- To test that the logs are correctly streamed, we check that actual
     -- order of logs against the timing logs. This is a bit hacky, but the
     -- sleep of 1.5 makes this fairly reliable.
     expecteds =
-      [ withCommandPrefix "for i in {0...." "Starting...",
-        withCommandPrefix "for i in {0...." "0",
+      [ withCommandPrefix "for i in 0 1..." "Starting...",
+        withCommandPrefix "for i in 0 1..." "0",
         withTimerPrefix "1 second",
-        withCommandPrefix "for i in {0...." "1",
+        withCommandPrefix "for i in 0 1..." "1",
         withTimerPrefix "2 seconds",
-        withCommandPrefix "for i in {0...." "2",
+        withCommandPrefix "for i in 0 1..." "2",
         withTimerPrefix "3 seconds",
         withTimerPrefix "4 seconds",
-        withCommandPrefix "for i in {0...." "3",
+        withCommandPrefix "for i in 0 1..." "3",
         withTimerPrefix "5 seconds",
-        withSuccessPrefix "for i in {0...." <> "6 seconds",
+        withSuccessPrefix "for i in 0 1..." <> "6 seconds",
         withFinishedPrefix "6 seconds"
       ]
 
