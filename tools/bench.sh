51f072feb54c33dfb64ebcd38ee4eb60ebba3317
Tommy Bidne <tbidne@protonmail.com>
2025-12-18 12:43:09 +1300

    Remove baseline from benchmarks

--- tools/bench.sh
+++ tools/bench.sh
@@ -3,4 +3,4 @@ set -e
 export LANG="C.UTF-8"
 
 cabal bench --benchmark-options \
-  '+RTS -T -RTS -t100 --csv benchmarks/bench.csv --svg benchmarks/bench.svg'
+  '+RTS -T -RTS -t200 --csv benchmarks/bench.csv --svg benchmarks/bench.svg'
